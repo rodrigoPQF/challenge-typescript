95f488594c4065233d038400094abfb2c55753d5
Rodrigo Pereira <rodrigopr404@gmail.com>
2023-03-14 15:51:02 -0300

    🚧 BooleanTypes

---
+++ src/modules/booleanTypes.ts
@@ -0,0 +1,18 @@
+
+// ==> Exemplo 01
+
+let tarefaConcluida: boolean = true;
+let tarefaPendente: boolean = false;
+
+console.log(tarefaConcluida, tarefaPendente);
+
+// ==> Exemplo 02
+
+let concluido: boolean = true;
+
+if(concluido){
+  console.log('Tarefa concluida');
+}else {
+  console.log('Tarefa pendente');
+  
+}
