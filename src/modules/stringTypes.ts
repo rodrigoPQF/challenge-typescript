88a73e28eedf5d4557641d17e79f1ace490d9cef
Rodrigo Pereira <rodrigopr404@gmail.com>
2023-03-14 16:03:14 -0300

    🚧 String types

---
+++ src/modules/stringTypes.ts
@@ -0,0 +1,11 @@
+
+// ==> Exemplo 01 - Single Quotes
+let nomeCompleto: string = 'Rodrigo Pereira'
+console.log(nomeCompleto)
+
+// ==> Exemplo 02 - Double Quotes
+let nomeAnimal: string = "Elefante"
+
+// ==> Exemplo 03 - Template Strings
+
+let url: string = `${nomeAnimal} é esse`
