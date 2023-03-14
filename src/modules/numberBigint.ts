272a6965758c6f5b825e344bb62d4c7a2092a4c5
Rodrigo Pereira <rodrigopr404@gmail.com>
2023-03-14 15:51:16 -0300

    🚧 Number and BigInt

---
+++ src/modules/numberBigint.ts
@@ -0,0 +1,13 @@
+
+
+// ==> Exemplos: number
+let numero: number = 23.4
+let numeroHexa: number = 0x3949233
+
+console.log(numero, numeroHexa);
+
+// ==> Exemplos: bigint
+
+let big1: bigint = 329293923923n;
+let big2: bigint = 0b1000000000000000000000000000000000000000n;
+console.log(big1, big2);
